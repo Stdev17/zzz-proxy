35545b7f4b320e338f46a11a2ae6c0914acc7f6c
Min Yoon <contact@berylleta.dev>
2024-08-07 17:43:06 +0900

    chore: Fix build settings for Amplify

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	root: 'src',
+	build: {
+		outDir: '../build',
+		emptyOutDir: true, // also necessary
 	}
 });
